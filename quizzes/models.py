0c2ae9be45bf692c79b42a24931fe07684812dc9
Praveen Kumar Nirmalkar <pknirmalkar96@gmail.com>
2024-08-09 00:18:25 +0530

    feat(app): add question and choice models

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField("question", max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse("questions:question", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f"Question: {self.text}"
+
+    def __repr__(self):
+        return f"Question ({self.pk}): {self.text}"
+
+    class Meta:
+        ordering = ("pk",)
+        verbose_name = "Question"
+        verbose_name_plural = "Quiz Questions"
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name="choices",
+    )
+    text = models.TextField("choice")
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Choice: {self.text}"
+
+    def __repr__(self):
+        return f"Choice ({self.pk}): {self.text}"
